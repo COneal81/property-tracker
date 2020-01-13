20b48abaeec7d142301d3eeab16f0733291f824a
github username <github email address>
2020-01-13 14:32:51 -0500

    Added activerecord associations for payment and rental property

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,4 @@
 class Payment < ApplicationRecord
+    belongs_to: :rental_property
+    has_many :tentants, through: :rental_properties
 end
--- app/models/rental_property.rb
+++ app/models/rental_property.rb
@@ -1,6 +1,6 @@
 class RentalProperty < ApplicationRecord
     belongs_to :user
-    has_many :repairs
     has_many :tentants
-    has_many :payments, through: :tenants
+    has_many :repairs
+    has_many :payments, through: :tentants
 end
