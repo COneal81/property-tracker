d7063c77c6b9b87f676ead6e724ae7a3b518abf6
github username <github email address>
2020-01-21 16:06:49 -0500

    Created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+RentalProperty.destroy_all
+Tenant.destroy_all
+Payment.destroy_all
+Repair.destroy_all
+
+
 
 #User
 kevin = User.find_or_create_by(email: "k@k.com", password: "test123")
@@ -12,42 +19,68 @@ wyatt = User.find_or_create_by(email: "w@w.com", password: "test123")
 
 #Rental Property
 
-red_house = RentalProperty.find_or_create_by(property_name: "", description: "", address: "", monthly_rental_amount: "", 
-deposit_amount: "", square_feet: "", leased: "", contract_start_date: "", contract_end_date: "", user: "")
+red_ranch = RentalProperty.find_or_create_by(property_name: "Red Ranch", description: "Spacious ranch home with a Texas feel!  5 Bed/4 Bath!", 
+address: "1234 Main St. Countyline, NC 22222", monthly_rental_amount: "1500", 
+deposit_amount: "1500", square_feet: "2800", leased: "true", contract_start_date: "12/1/19", contract_end_date: "12/1/20", user: kevin.id, 
+image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:HouseII2007.jpg")
 
-green_house = RentalProperty.find_or_create_by(property_name: "", description: "", address: "", monthly_rental_amount: "", 
-deposit_amount: "", square_feet: "", leased: "", contract_start_date: "", contract_end_date: "", user: "")
+cape_cod = RentalProperty.find_or_create_by(property_name: "Cape Cod", description: "Cute and cozy!  2 Bed ~ 1.5 Bath", 
+address: "123 Liberty Rd Liberty, NC 22221", monthly_rental_amount: "800", 
+deposit_amount: "800", square_feet: "1500", leased: "false", contract_start_date: "N/A", contract_end_date: "N/A", user: kevin.id, 
+image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:Cape_Cod_style_house.jpg")
 
-blue_house = RentalProperty.find_or_create_by(property_name: "", description: "", address: "", monthly_rental_amount: "", 
-deposit_amount: "", square_feet: "", leased: "", contract_start_date: "", contract_end_date: "", user: "")
+blue_bungalow = RentalProperty.find_or_create_by(property_name: "Blue Bungalow", description: "Front porch living at its best!  4 Bed ~ 3 
+Bath", address: "456 Market St Coffee, NC 22254", monthly_rental_amount: "1100", 
+deposit_amount: "1500", square_feet: "2490", leased: "true", contract_start_date: "1/1/20", contract_end_date: "2/1/21", user: wyatt.id, 
+image_url: "https://en.wikipedia.org/wiki/List_of_house_styles#/media/File:Abernathy-Shaw_House_c.1908.jpg")
 
-tan_house = RentalProperty.find_or_create_by(property_name: "", description: "", address: "", monthly_rental_amount: "", 
-deposit_amount: "", square_feet: "", leased: "", contract_start_date: "", contract_end_date: "", user: "")
+farm_house = RentalProperty.find_or_create_by(property_name: "Farm House", description: "Nestled on 20 acres, this farm house is 
+surraounded by whispering pines.  5 Bed ~ 4.5 Bath  Includes a 10 stall horse ban and 5 pastures.", 
+address: "7899 Hidden Valley Rd Farmland, NC 22298", monthly_rental_amount: "2600", 
+deposit_amount: "3000", square_feet: "3500", leased: "true", contract_start_date: "7/7/19", contract_end_date: "7/7/20", user: wyatt.id, 
+mage_url: "https://en.wikipedia.org/wiki/Farm#/media/File:Farming_near_Klingerstown,_Pennsylvania.jpg")
 
-#Tentant
-kari = Tentant.find_or_create_by(name_1: "", name_2: "", name_3: "", address: "", email_1: "", email_2: "", email_3: "", 
-phone_1: "", phone_2: "", phone_3: "", payment_date: "", deposit_amount_paid: "", monthly_rental_amount_due: "", rental_property_id: "")
 
-hudson = Tentant.find_or_create_by(name_1: "", name_2: "", name_3: "", address: "", email_1: "", email_2: "", email_3: "", 
-phone_1: "", phone_2: "", phone_3: "", payment_date: "", deposit_amount_paid: "", monthly_rental_amount_due: "", rental_property_id: "")
 
-griffin = Tentant.find_or_create_by(name_1: "", name_2: "", name_3: "", address: "", email_1: "", email_2: "", email_3: "", 
-phone_1: "", phone_2: "", phone_3: "", payment_date: "", deposit_amount_paid: "", monthly_rental_amount_due: "", rental_property_id: "")
+#Tenant
+kari = Tenant.find_or_create_by(renter: "Kari Smith", co_renter: "N/A", address: "210 Cesna Lane Airport, IA 44444", 
+renter_email: "K@K.com", co_renter_email: "N/A", renter_cell_phone: "123-456-7890", 
+co_renter_cell_phone: "N/A", payment_due_date: "5th", deposit_amount_paid: "3000", monthly_rental_amount_due: "2600", 
+rental_property_id: farm_house.id)
 
+hudson = Tenant.find_or_create_by(renter: "Hudson Jones", co_renter: "Hunter Jones", address: "2222 One Lane Rd Somewhere, OK 58949", 
+renter_email: "hud@hud.com", co_renter_email: "hunt@hunt.com", renter_cell_phone: "234-567-8901", 
+co_renter_cell_phone: "567-890-1234", payment_due_date: "15th", deposit_amount_paid: "1500", monthly_rental_amount_due: "1100", 
+rental_property_id: blue_bungalow.id)
+
+griffin = Tenant.find_or_create_by(renter: "Griffin Williams", co_renter: "Massey Williams", address: "4999 Slick Rd Snow, MN 58588", 
+renter_email: "griff@griff.com", co_renter_email: "mass@mass.com", renter_cell_phone: "987-987-9870", 
+co_renter_cell_phone: "987-543-2345", payment_due_date: "1st", deposit_amount_paid: "1500", monthly_rental_amount_due: "1500", 
+rental_property_id: red_ranch.id)
+
+ 
 
 
 #Payment
 
-pay_1 = Payment.find_or_create_by(amount_owed: "", amount_paid: "", due_date: "", rental_property_id: "")
-pay_2 = Payment.find_or_create_by(amount_owed: "", amount_paid: "", due_date: "", rental_property_id: "")
-pay_3 = Payment.find_or_create_by(amount_owed: "", amount_paid: "", due_date: "", rental_property_id: "")
-pay_4 = Payment.find_or_create_by(amount_owed: "", amount_paid: "", due_date: "", rental_property_id: "")
+pay_1 = Payment.find_or_create_by(amount_owed: "1500", amount_paid: "1500", due_date: "1st", rental_property_id: red_ranch.id)
+pay_2 = Payment.find_or_create_by(amount_owed: "1500", amount_paid: "1500", due_date: "1st", rental_property_id: red_ranch.id)
+pay_3 = Payment.find_or_create_by(amount_owed: "1100", amount_paid: "1000", due_date: "15th", rental_property_id: blue_bungalow.id)
+pay_4 = Payment.find_or_create_by(amount_owed: "2600", amount_paid: "2000", due_date: "5th", rental_property_id: farm_house.id)
 
 
 
 #Repair
 
-repair_1 = Repair.find_or_create_by(repair_needed: "", description: "", repair_cost: "", repair_completed: "", rental_property_id:"")
-repair_2 = Repair.find_or_create_by(repair_needed: "", description: "", repair_cost: "", repair_completed: "", rental_property_id:"")
-repair_3 = Repair.find_or_create_by(repair_needed: "", description: "", repair_cost: "", repair_completed: "", rental_property_id:"")
-repair_4 = Repair.find_or_create_by(repair_needed: "", description: "", repair_cost: "", repair_completed: "", rental_property_id:"")
+repair_1 = Repair.find_or_create_by(repair_needed: "true", repair_description: "Broken lock on back door", repair_cost: "50", 
+repair_completed: "false", rental_property_id:cape_cod.id)
+
+repair_2 = Repair.find_or_create_by(repair_needed: "true", repair_description: "New roof", repair_cost: "12000", 
+repair_completed: "true", rental_property_id:farm_house.id)
+
+repair_3 = Repair.find_or_create_by(repair_needed: "true", repair_description: "Outside needs painted", repair_cost: "2800", 
+repair_completed: "false", rental_property_id: blue_bungalow.id)
+
+repair_4 = Repair.find_or_create_by(repair_needed: "true", repair_description: "New window on south side of home.  Broken by tenant.  
+Bill tenant", repair_cost: "1000", 
+repair_completed: "false", rental_property_id: red_ranch.id)
