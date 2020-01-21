c3f3d4bc2aafae77b27b26aa21739e9e311ea7c8
github username <github email address>
2020-01-21 13:03:34 -0500

    Added actions for users and sessions controller, drew routes for users and sessions, created user/new form for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.create(user_params)
+        redirect_to rental_properties_path
+    end
+
+    def show
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   
+
+  root to: 'welcome#index'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :payments
   resources :tentants
   resources :repairs
   resources :rental_properties
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-    root to: 'welcome#index'
+    
 
     # get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
     # get ‘auth/failure’, to: redirect(‘/’)
 
-    resources :users
+    
+    
 end
