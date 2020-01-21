1fa700fb048bb8a1753be8c5ced9c246c8dc54d2
github username <github email address>
2020-01-21 17:08:36 -0500

    Updated method create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,29 @@
 class UsersController < ApplicationController
-
+    
     def new 
         @user = User.new
     end
 
     def create 
-        @user = User.create(user_params)
-        redirect_to rental_properties_path
+        @user = User.new(user_params)
+        if @user.save
+            
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
 
     def show
-
+        find_user
     end
 
     private
+
+    def find_user
+        @user = User.find_by(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:email, :password, :password_confirmation)
