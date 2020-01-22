c5d066324516b74c4943ab2a3f65eeea965779ab
github username <github email address>
2020-01-21 21:06:06 -0500

    Added before_action

--- app/controllers/rental_properties_controller.rb
+++ app/controllers/rental_properties_controller.rb
@@ -1,4 +1,8 @@
 class RentalPropertiesController < ApplicationController
+
+    before_action :authenticate_user!
+    before_action :redirect_if_not_admin?, only: [:new, :create]
+
     def index
     end 
 # Full crud actions
