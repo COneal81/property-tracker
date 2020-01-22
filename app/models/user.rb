86ba91c301ee99409c97e1a24d05daf83fc8414f
github username <github email address>
2020-01-21 21:09:24 -0500

    Organized file for dev ux

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password_digest, presence: true, length: { in: 4..20 }
 
-  has_secure_password
-
   has_many :rental_properties
   
   has_many :tenants, through: :rental_properties
   has_many :repairs, through: :rental_properties
   has_many :payments, through: :rental_properties
+
+  has_secure_password
 end
