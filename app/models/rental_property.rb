cc3e0eb9e7c23840c973be78a379fcf69a96b7fb
github username <github email address>
2020-01-21 21:07:40 -0500

    Corrected typo throught, changed to through

--- app/models/rental_property.rb
+++ app/models/rental_property.rb
@@ -2,5 +2,5 @@ class RentalProperty < ApplicationRecord
     belongs_to :user
     has_many :repairs
     has_many :tenants
-    has_many :payments, throught: :tenants
+    has_many :payments, through: :tenants
 end
