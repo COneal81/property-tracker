04b28e8b237197c1acac6e64fbf401cb9f710232
github username <github email address>
2020-01-21 21:00:55 -0500

    Added helper methods current_user, is_logged_in?, authenticate_user!, is_admin?, redirect_if_not_admin!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
 
+    helper_method :current_user, :is_logged_in?, :authenticate_user!, :is_admin?
+
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def is_logged_in?
+        !!current_user
+    end
     
+    def authenticate_user!
+        redirect_to new_session_path if !is_logged_in?
+    end
+
+    def is_admin?
+        current_user.admin if current_user
+    end
+
+    def redirect__if_not_admin!
+        redirect__to rental_properties_path if !is_admin?
+    end
 end
