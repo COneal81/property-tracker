b2fafc819ba0ec6b9b205eb6aea9d7c08ff86ad7
github username <github email address>
2020-01-21 20:58:35 -0500

    Updated create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,17 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:email])
-
+        @user = User.find_by(email: params[:session][:email].downcase)
+        if @user && @user.authenticate(params[:session][:password])   
+            session[:user_id] = @user.id
+            redirect_to rental_properties_path
+        else
+            #place flash notice here (reference has_secure_password lab)
+            render :new
+        end
     end
 
     def destroy
-        
+
     end
 end
